f200321e6a5f769c8f150170c41bae88f093dc77
Anique Kumar Das <aniquedas512@gmail.com>
2025-07-28 02:25:26 +0600

    Add files via upload

---
+++ ifElse.cpp
@@ -0,0 +1,206 @@
+//Positive or negative number;
+
+/*#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<<"Enter any integer number:";
+    cin>>num;
+
+    if(num>0)
+    {
+        cout<<"Positive"<<endl;
+    }
+
+    else if(num<0)
+    {
+        cout<<"Negative"<<endl;
+
+    }
+
+    else
+    {
+        cout<<"Zero"<<endl;
+    }
+
+    return 0;
+}*/
+
+
+
+//Even or odd number
+
+/*#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<<"Enter any integer number:";
+    cin>>num;
+
+    if(num%2==0)
+    {
+        cout<<"Even";
+    }
+
+    else
+    {
+        cout<<"Odd";
+    }
+
+    return 0;
+}*/
+
+
+
+
+//Large or small number between 2 numbers
+
+/*#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+    cout<<"Enter two integer numbers:";
+    cin>>num1>>num2;
+
+    if(num1>num2)
+    {
+        cout<<"Number 1 is the largest";
+    }
+
+    else
+    {
+        cout<<"Number 2 is the largest";
+    }
+
+    return 0;
+}*/
+
+
+
+
+// AIUB letter grade
+
+/*#include<iostream>
+using namespace std;
+
+int main()
+{
+    int mark;
+    cout<<"Enter the mark:";
+    cin>>mark;
+
+    if(mark>=90 && mark<=100)
+    {
+       cout<<"A+";
+    }
+
+    else if(mark>=85 && mark<=89)
+    {
+        cout<<"A";
+    }
+
+    else if(mark>=80 && mark<=84)
+    {
+        cout<<"B+";
+    }
+
+    else if(mark>=75 && mark<=79)
+    {
+        cout<<"B";
+    }
+
+    else if(mark>=70 && mark<=74)
+    {
+        cout<<"C+";
+    }
+
+    else if(mark>=65 && mark<=69)
+    {
+        cout<<"C";
+    }
+
+    else if(mark>=60 && mark<=64)
+    {
+        cout<<"D+";
+    }
+
+    else if(mark>=50 && mark<=59)
+    {
+        cout<<"D";
+    }
+
+    else
+    {
+        cout<<"Fail";
+    }
+
+    return 0;
+}*/
+
+
+
+//Absolute value
+
+/*#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<<"Enter any number:";
+    cin>>num;
+
+    if(num<0)
+    {
+        cout<<"Absolute value is: "<<(-num)<<endl;
+    }
+
+    else
+    {
+        cout<<"Absolute value is: "<<num<<endl;
+    }
+
+  return 0;
+}*/
+
+
+
+//Pass or fail
+
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int mark;
+    cout<<"Enter the mark:";
+    cin>>mark;
+
+    if(mark>=50 && mark<=100)
+    {
+        cout<<"Pass";
+    }
+
+    else if(mark>=0 && mark<50)
+    {
+        cout<<"Fail";
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
