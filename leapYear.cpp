7679f8f02384855f50a8f735dfec2c5551debf99
Anique Kumar Das <aniquedas512@gmail.com>
2025-07-29 22:54:45 +0600

    Add files via upload

---
+++ leapYear.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int year;
+    cout<<"Enter the year:";
+    cin>>year;
+
+    if(year%4 == 0 && year % 100!=0)
+    {
+        cout<<"Leap Year";
+    }
+
+    else if(year%400 == 0)
+    {
+        cout<<"Leap Year";
+    }
+
+    else
+    {
+        cout<<"Not Leap Year";
+    }
+    return 0;
+}
