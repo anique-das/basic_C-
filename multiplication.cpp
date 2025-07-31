066acc5890f14c9202117832cadc46f0802f616c
Anique Kumar Das <aniquedas512@gmail.com>
2025-07-31 12:37:15 +0600

    Add files via upload

---
+++ multiplication.cpp
@@ -0,0 +1,20 @@
+// Multiplication Table
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<<"Enter any integer number:";
+    cin>>num;
+
+
+    for(int i=1;i<=10;i++)
+    {
+        cout<<num<<" * "<<i<<" = "<<(num*i)<<endl;
+    }
+
+    return 0;
+}
+
